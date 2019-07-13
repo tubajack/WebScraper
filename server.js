bd798b8e767c755af2ee1b5ffa069577e80b2810
Jackson Henry <jackson.henry13@yahoo.com>
2019-07-13 15:29:58 -0500

    Also have included morgan. This is the HTTP request logger middleware for Node.js.

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var express = require("express");
+var logger = require("morgan");
 var mongoose = require("mongoose");
 
 var axios = require("axios");
 var cheerio = require("cheerio");
+
+//Make sure we require all of the models 
+var db = require("./models");
+
