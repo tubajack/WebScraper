80ece64697f7c507346bb45e51649f69b5b41a56
Jackson Henry <jackson.henry13@yahoo.com>
2019-07-13 20:21:06 -0500

    Will be necessary to create the ArticleSchema model. Mongoose has provided a model method.

--- models/Article.js
+++ models/Article.js
@@ -1,8 +1,11 @@
 var mongoose = require("mongoose");
 
-//Reference the Schema cnstructor. 
+//Reference the Schema constructor. 
 //This is fairly similar to sequeize.
 var Schema = mongoose.Schema; 
+
+//Create our model from the schema, using the model method provided by mongoose
+var Article = mongoose.model("Article", ArticleSchema);
 
 //Export the Article module when finished
 module.exports = "Article";
