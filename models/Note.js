1143daf87ada07f98ee4b1b1995565978a5d0cbb
Jackson Henry <jackson.henry13@yahoo.com>
2019-07-13 20:39:04 -0500

    The Note.js file is pretty similar to the Article.JS file. Right now, I am just referencing the Schema constructor.

--- models/Note.js
+++ models/Note.js
@@ -1,4 +1,7 @@
 var mongoose = require("mongoose");
 
+//Do not forget to reference the Schema constructor
+var Schema = mongoose.Schema;
+
 //Export the note model when finished
 module.exports = Note;
